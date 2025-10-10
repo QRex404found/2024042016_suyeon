05ac0f81f17d7f850c3644bee24ae92650ec320d
SuYeon6323 <susan6323@naver.com>
2025-10-10 11:57:22 +0900

    (3) task

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -4,18 +4,23 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
+@RequestMapping("/question")
 @RequiredArgsConstructor
 @Controller
 public class QuestionController{
 	
 	private final QuestionService questionService;
 	
-	@GetMapping("/question/list")
+	@GetMapping("/List")
 
 	public String List(Model model) {
 		List<Question> questionList = this.questionService.findAll();
@@ -23,10 +28,22 @@ public class QuestionController{
 		return "question_list";
 	}
 	
-	@GetMapping(value = "/question/detail/{id}")
+	@GetMapping(value = "/detail/{id}")
 	public String detail(Model model, @PathVariable("id") Integer id) {
 		Question question = this.questionService.getQuestion(id);
 		model.addAttribute("question",question);
 		return "question_detail";
+	}
+	
+	@GetMapping("/create")
+	public String questionCreate() {
+		return "question_form";
+	}
+	
+	@PostMapping("/create")
+	public String questionCreate(@RequestParam(value="subject") String subject,
+	@RequestParam(value="content") String content) {
+		this.questionService.create(subject, content);
+		return "redirect:/question/list";
 	}
 }
--- src/main/java/com/mysite/sbb/question/QuestionService.java
+++ src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -1,10 +1,13 @@
 package com.mysite.sbb.question;
 
 import java.util.List;
+import java.util.Optional;
 
+import com.mysite.sbb.DataNotFoundException;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+
 
 @RequiredArgsConstructor
 @Service
@@ -14,5 +17,14 @@ public class QuestionService{
 	
 	public List<Question> getList(){
 		return this.questionRepository.findAll();
+	}
+	
+	public Question getQuestion(Integer id) {
+		Optional‹Question> question = this-questionRepository.findById(id);
+		if (question.isPresent) {
+			return question.getO;
+		} else {
+			throw new DataNotFoundException("question not found");
+		}
 	}
 }
